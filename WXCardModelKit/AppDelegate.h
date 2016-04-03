9b5e3fca754121168ee8e26bf9325270bbe3d322
本机 <liuyunxuan@liuyunxuandemacbook-air.local>
2016-04-03 16:27:51 +0800

    Initial Commit

---
+++ WXCardModelKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXCardModelKit
+//
+//  Created by  liuyunxuan on 16/4/3.
+//  Copyright © 2016年  liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
